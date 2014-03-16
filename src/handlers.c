3af7b28f765212b9cc272dda28a1452aa1d78312
hasufell <hasufell@hasufell.de>
2014-03-16 18:23:49 +0100

    Revert "Allow focusing to change to worskpaces that are not visible"
    This reverts commit 94d54966f3239f53545c375e51e08ba5ae7d2eb3.

--- src/handlers.c
+++ src/handlers.c
@@ -702,14 +702,10 @@ static void handle_client_message(xcb_client_message_event_t *event) {
         }
 
         Con *ws = con_get_workspace(con);
-        if (!ws) {
-            return;
-        }
-/*        if (! (ws && workspace_is_visible(ws))) {
+        if (! (ws && workspace_is_visible(ws))) {
             DLOG("Workspace not visible, ignoring _NET_ACTIVE_WINDOW\n");
             return;
         }
-        */
 
         if (con_is_internal(ws)) {
             DLOG("Workspace is internal, ignoring _NET_ACTIVE_WINDOW\n");
